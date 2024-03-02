08064fb4ee394b2348f70dead03c6a3615c88888
Brassinolide <99178854+Brassinolide@users.noreply.github.com>
2024-03-02 18:56:30 +0800

    禁用更多无用服务

--- 禁用CompatTelRunner和Diagnostic服务.bat
+++ 禁用CompatTelRunner和Diagnostic服务.bat
@@ -2,10 +2,27 @@
 chcp 65001
 echo 需要以SYSTEM身份运行，确认后按任意键继续
 pause>nul
+
+::禁用CompatTelRunner
 schtasks /change /tn "\Microsoft\Windows\Application Experience\Microsoft Compatibility Appraiser" /disable
+
+::一些诊断服务
 reg add "HKLM\SYSTEM\ControlSet001\Services\DiagTrack" /v "Start" /t "REG_DWORD" /d "4" /f
 reg add "HKLM\SYSTEM\ControlSet001\Services\diagsvc" /v "Start" /t "REG_DWORD" /d "4" /f
 reg add "HKLM\SYSTEM\ControlSet001\Services\DPS" /v "Start" /t "REG_DWORD" /d "4" /f
 reg add "HKLM\SYSTEM\ControlSet001\Services\WdiServiceHost" /v "Start" /t "REG_DWORD" /d "4" /f
 reg add "HKLM\SYSTEM\ControlSet001\Services\WdiSystemHost" /v "Start" /t "REG_DWORD" /d "4" /f
+
+::应用程序兼容性助手
+reg add "HKLM\SYSTEM\ControlSet001\Services\PcaSvc" /v "Start" /t "REG_DWORD" /d "4" /f
+
+::脱机服务，缓存网络文件以便在脱机时访问，用不上
+reg add "HKLM\SYSTEM\ControlSet001\Services\CSC" /v "Start" /t "REG_DWORD" /d "4" /f
+
+::推送通知系统服务
+reg add "HKLM\SYSTEM\ControlSet001\Services\WpnService" /v "Start" /t "REG_DWORD" /d "4" /f
+
+::ipv6服务，纯ipv4可以关闭，根据需要修改
+reg add "HKLM\SYSTEM\ControlSet001\Services\iphlpsvc" /v "Start" /t "REG_DWORD" /d "4" /f
+
 pause
